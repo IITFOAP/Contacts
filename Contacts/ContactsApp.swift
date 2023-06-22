cab904e4a3b37f0b86db867fb093c6765131685e
Baranov Roman <palamarmle@gmail.com>
2023-06-22 22:07:48 +0300

    Initial Commit

---
+++ Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by Рома Баранов on 22.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
